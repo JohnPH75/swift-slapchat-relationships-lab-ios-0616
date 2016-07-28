b9ff5fb8b08b4b2129d270a6fcfc77e9a1297548
JohnPH75 <johnhussain75@gmail.com>
2016-07-28 09:02:52 -0400

    slapChatRelations

--- SlapChat/SlapChat/TableViewController.swift
+++ SlapChat/SlapChat/TableViewController.swift
@@ -9,8 +9,8 @@
 import UIKit
 
 class TableViewController: UITableViewController {
-
     
+    var messages: Set<Message> = []
     var managedMessageObjects: [Message] = []
     let store: DataStore = DataStore()
     
@@ -32,9 +32,10 @@ class TableViewController: UITableViewController {
     override func viewWillAppear(animated: Bool) {
         
         super.viewWillAppear(true)
-        
+    
         store.fetchData()
         tableView.reloadData()
+        
         
     }
     
@@ -51,15 +52,15 @@ class TableViewController: UITableViewController {
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return store.messages.count
+        return messages.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCellWithIdentifier("basicCell", forIndexPath: indexPath)
         
-        let eachMessage = store.messages[indexPath.row]
-        
+        let messagesArray = Array(messages)
+        let eachMessage = messagesArray[indexPath.row]
         cell.textLabel?.text = eachMessage.content
         
         return cell
